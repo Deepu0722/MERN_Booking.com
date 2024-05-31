648da4bf69f5ca7487af98a097f7f5ef01efc3af
Deepu0722 <deepu.03feb@gmail.com>
2024-05-31 13:17:18 -0400

    css changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html",  "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        blue:{
+          800: '#003b95',
+        },
+      },
+    },
     container: {
       padding: {
         md:"10rem",
